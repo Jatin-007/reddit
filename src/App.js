dcd47c4362cf929ec86cb37b13da4365985917b7
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-05-05 22:32:57 -0700

    Firebase setup for auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Landing from './components/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <Landing/>
         </header>
       </div>
     );
