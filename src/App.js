7960f64aa3e2822f862a22979d8ec93ce5bf871f
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-05-08 16:01:20 -0700

    minor change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import Navigation from './components/Navigation';
-import {BrowserRouter as Router} from 'react-router-dom';
+import Landing from './components/Landing';
+
+import * as routes from './constants/routes';
 
 class App extends Component {
   render() {
@@ -8,6 +12,8 @@ class App extends Component {
       <div className="container">
         <Router>
           <Navigation/>
+
+          <hr/>
         </Router>
       </div>
     );
