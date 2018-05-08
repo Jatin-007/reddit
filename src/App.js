629f1926f3a9e402e1b7083aa29405d62067c798
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-05-08 16:12:59 -0700

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
+import * as routes from './constants/routes';
+
 import Navigation from './components/Navigation';
 import Landing from './components/Landing';
-
-import * as routes from './constants/routes';
+import SignUp from './components/Signup';
+import SignIn from './components/SignIn';
+import PasswordForget from './components/PasswordForget';
+import Home from './components/Home';
+import Account from './components/Account';
 
 class App extends Component {
   render() {
@@ -12,8 +17,14 @@ class App extends Component {
       <div className="container">
         <Router>
           <Navigation/>
-
           <hr/>
+
+          <Route exact path={routes.HOME} component = {()=> <Home/>}/>
+          <Route exact path={routes.SIGN_UP} component = {()=> <SignUp/>}/>
+          <Route exact path={routes.SIGN_IN} component = {()=> <SignIn/>}/>
+          <Route exact path={routes.LANDING} component = {()=> <Landing/>}/>
+          <Route exact path={routes.ACCOUNT} component = {()=> <Account/>}/>
+          <Route exact path={routes.PASSWORD_FORGET} component = {()=> <PasswordForget/>}/>
         </Router>
       </div>
     );
